9bf4c8ae9e3c1dd5cd6e8d4b537292b8e771a603
Ravi Semwal <ravi.semwal@infobeans.com>
2016-12-16 19:14:26 +0530

    Removed junk

--- server/services/os/JSONGenerators/bootstrap.js
+++
@@ -1,7 +0,0 @@
-var ubuntu=require ('./UbuntuJsonGenerator.js');
-
-var primary_json_generator={
-	'ubuntu':ubuntu
-}
-
-module.exports = primary_json_generator
--- server/services/os/operating_system.js
+++
@@ -1,8 +0,0 @@
-var ubuntu=require ('./ubuntu');
-var terminal={
-'ubuntu':ubuntu,
-}
-var operating_system={
-'terminal':terminal
-}
-module.exports = operating_system
